8cb3abccc4c738db7e5cf18a5fbc7f4173d8128b
DJDevon3 <49322231+DJDevon3@users.noreply.github.com>
2023-09-06 13:28:12 -0400

    Landscape Mode update
    Now defaults to landscape refresh mode

--- circuitpython_st7796.py
+++ circuitpython_st7796.py
@@ -1,19 +1,18 @@
 # SPDX-FileCopyrightText: 2023 DJDevon3
 # SPDX-License-Identifier: MIT
-# Generic Circuit Python Driver for ST7796x based displays
+# Circuit Python Driver for ST7796S display
 
 import displayio
 
 
 _INIT_SEQUENCE = (
-    b"\x01\x80\x96"  # _SWRESET and Delay 150ms
-    b"\x11\x80\xFF"  # _SLPOUT and Delay 500ms
-    b"\x3A\x81\x55\x0A"  # _COLMOD and Delay 10ms
-    b"\x36\x01\x48"  # _MADCTL
-    b"\x20\x80\x0A"  # _INVON Hack and Delay 10ms
-    b"\x15\x80\x0A"  # _NORON and Delay 10ms
-    b"\x36\x01\x48"  # _MADCTL
-    b"\x29\x80"  # _DISPON and Delay 500ms
+    b"\x01\x80\x01"  # Software reset and Delay 150ms
+    b"\x11\x80\x01"  # Sleep out and Delay 500ms
+    b"\x3A\x81\x55\x0A"  # Set color mode to 16 bits per pixel and Delay 10ms
+    b"\x36\x01\x28"  # Set MADCTL for landscape mode
+    b"\x20\x80\x01"  # Turn off inversion and Delay 10ms
+    b"\x13\x80\x01"  # Normal display mode on and Delay 10ms
+    b"\x29\x80\x01"  # Display on and Delay 500ms
 )
 
 
